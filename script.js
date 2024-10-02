12bfad03ffdedf88595fe94a449c895875f63761
Helleus <160227924+Helleus@users.noreply.github.com>
2024-10-02 04:13:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let inputText = document.getElementById("input-box");
+let listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputText.value === "") {
+    // alert("Add Your Text");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputText.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputText.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
